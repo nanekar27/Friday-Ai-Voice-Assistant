cd240d41b09eb5393f8be6ed2a6dc5b939a65bde
nanekar27 <119592291+nanekar27@users.noreply.github.com>
2024-02-24 19:27:48 +0530

    Add files via upload

---
+++ Features/Wakeup.py
@@ -0,0 +1,23 @@
+import speech_recognition as sr
+
+def Listen():
+
+    r = sr.Recognizer()
+
+    with sr.Microphone() as source:
+        print("Listening...")
+        r.pause_threshold = 1
+        audio = r.listen(source,0,7) # Listening Mode.....
+    
+    try:
+        print("Recognizing...")
+        query = r.recognize_google(audio,language="en")
+
+    except:
+        return ""
+    
+    query = str(query).lower()
+    print(query)
+    return query
+
+
